39a5fb14409b85c8306ed210457410d9fb080953
Milou Sanden <Milou.Sanden@gmail.com>
2025-08-29 13:14:31 +0200

    did load csv file

--- Code_along/06_extract_load_csv_dlt/dlt_code/load_csv.py
+++ Code_along/06_extract_load_csv_dlt/dlt_code/load_csv.py
@@ -5,6 +5,9 @@ import os
 
 
 #used for extracting data from source, in this case a local csv file
+# använder för att visa vilken data vi vill ha som datasource.
+# vi declarar en funktion som läser in csv filen och returnerar en dataframe.
+# 1. dlt resource objekt. 2. read csv file med pandas. 3. yeild dataframe.
 @dlt.resource(write_disposition="replace")
 def load_csv_resource(file_path: str, **kwargs):
     df = pd.read_csv(file_path, **kwargs)
@@ -24,12 +27,15 @@ if __name__ == "__main__":
     csv_path = working_directory / "data" / "NetflixOriginals.csv"
     data = load_csv_resource(csv_path, encoding="latin1")
     print(data)
+    # skapar en dlt pipeline som laddar data till snowflake. dlt.pipline är funktionen som skapar piplinen, den letar under secrets.toml efterdestination och credentials.
+    # vi ger även pipleninen ett namn och datasetet ett namn.
     pipeline = dlt.pipeline(
         pipeline_name='movies',
         destination="snowflake",
         dataset_name='staging'
         )
     
+    # kör piplinen med data som vi vill ladda och ger tabellen ett namn. 
     load_info = pipeline.run(data, table_name="netflix")
 
     # pretty print the information on data that was loaded
